dad6e2fffb2b9e3ddcd81e20567083e2010cfd3f
iconsumeplutonium <uzymc242@outlook.com>
2025-05-24 15:20:24 -0700

    dynamically generated controls based on files array

--- main.js
+++ main.js
@@ -12,29 +12,44 @@ let renderer, scene, camera;
 let controls;
 
 let selectedIndex = 0;
-const files = [
-    {
-        fileName: 'data/testcloud.txt',
-        displayText: document.getElementById("test"),
-        checkbox: document.getElementById("testcloudCheckbox"),
-        pointCloud: NaN,
-    },
-    {
-        fileName: 'data/apt-seq131.txt',
-        displayText: document.getElementById("131"),
-        checkbox: document.getElementById("131checkBox"),
-        pointCloud: NaN,
-    },
-    {
-        fileName: 'data/apt-seq136.txt',
-        displayText: document.getElementById("136"),
-        checkbox: document.getElementById("136checkBox"),
-        pointCloud: NaN,
-    },
-]
+// const files = [
+//     {
+//         fileName: 'data/testcloud.txt',
+//         displayText: document.getElementById("test"),
+//         checkbox: document.getElementById("testcloudCheckbox"),
+//         pointCloud: NaN,
+//     },
+//     {
+//         fileName: 'data/apt-seq131.txt',
+//         displayText: document.getElementById("131"),
+//         checkbox: document.getElementById("131checkBox"),
+//         pointCloud: NaN,
+//     },
+//     {
+//         fileName: 'data/apt-seq136.txt',
+//         displayText: document.getElementById("136"),
+//         checkbox: document.getElementById("136checkBox"),
+//         pointCloud: NaN,
+//     },
+//     {
+//         fileName: 'data/myformat.txt',
+//         displayText: document.getElementById("da2"),
+//         checkbox: document.getElementById("da2box"),
+//         pointCloud: NaN,
+//     },
+// ]
+
+const inputFiles = [
+    'data/testcloud.txt',
+    'data/apt-seq131.txt',
+    'data/apt-seq136.txt',
+    'data/myformat.txt',
+];
+let files = [];
 
 
 function main() {
+    // force canvas to be fullscreen
     document.documentElement.style.height = '100vh';
     document.body.style.height = '100vh';
     document.body.style.margin = '0';
@@ -42,6 +57,44 @@ function main() {
     document.documentElement.style.height = '100%';
     document.body.style.height = '100%';
     document.body.style.padding = '0';
+
+    // create controls for each file dynamically
+    const controlDiv = document.getElementById("control-div");
+    inputFiles.forEach((file, index) => {
+        const fileName = file.split('/').pop().replace(/\.[^/.]+$/, "");
+        
+        // parent div
+        const fileControl = document.createElement('div');
+        fileControl.style.display = 'flex';
+        fileControl.style.flexDirection = 'row';
+        fileControl.style.alignItems = 'center';
+        fileControl.style.margin = '2px 0';
+        
+        // checkbox
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `${fileName}Checkbox`;
+        checkbox.checked = false;
+        
+        // label
+        const text = document.createElement('p');
+        text.id = fileName;
+        text.style.margin = '0 0 0 5px';
+        text.textContent = `(${index + 1}) ${fileName}`;
+        
+        fileControl.appendChild(checkbox);
+        fileControl.appendChild(text);
+        controlDiv.appendChild(fileControl);
+        
+        files[index] = {
+            fileName: file,
+            displayText: text,
+            checkbox: checkbox,
+            pointCloud: NaN
+        };
+    });
+
+
 
     // renderer
     const canvas = document.querySelector("#webgl");
@@ -52,7 +105,7 @@ function main() {
     camera = new three.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 2;
 
-    window.addEventListener('resize', function() {
+    window.addEventListener('resize', function () {
         renderer.setSize(window.innerWidth, window.innerHeight);
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
@@ -71,8 +124,9 @@ function main() {
 
     // switch between scenes
     document.addEventListener('keypress', function (ev) {
-        if (!["1", "2", "3"].includes(ev.key)) return;
+        if (!/^[0-9]$/.test(ev.key)) return;
         const index = Number(ev.key) - 1;
+        if (index >= files.length) return
 
         files[index].checkbox.checked = !files[index].checkbox.checked;
         updateSceneVisibility();
